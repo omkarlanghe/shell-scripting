f7fd24359d5d259037e58a848d47d3a2a7c85e5a
omkarlanghe <langhe.omkar96@gmail.com>
2020-08-03 13:39:08 +0530

    first shell script

---
+++ first-script.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a comment!
+echo "Hello	world" # This is the comment too.
+echo "Hello world"
+echo "Hello * world"
+echo Hello * world
+echo Hello 	world
+echo "Hello" world
+echo "Hello "	 " world"
+echo "Hello"*"world"
+echo `hello` world
+echo `hello' world
