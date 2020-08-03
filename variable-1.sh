af27195c2eabdf9f996e2c4383fcb655fefc7dc1
omkarlanghe <langhe.omkar96@gmail.com>
2020-08-03 14:04:22 +0530

    variables

---
+++ variable-1.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+MY_MESSAGE="Hello world"
+MY_SHORT_MESSAGE=hi
+MY_NUMBER=1
+MY_PI=3.142
+MY_OTHER_PI="3.142"
+MY_MIXED=123abcd
+echo $MY_MESSAGE
+echo $MY_SHORT_MESSAGE
+echo $MY_NUMBER
+echo $MY_PI
+echo $MY_OTHER_PI
+echo $MY_MIXED
+expr $MY_NUMBER + 20
